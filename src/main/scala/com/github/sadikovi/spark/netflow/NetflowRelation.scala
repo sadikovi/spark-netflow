697db0f58e146aa170fa51e039b35f0ae56795d6
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2016-01-25 19:23:18 +1300

    fixed tests, removed short name for netflow

--- src/main/scala/com/github/sadikovi/spark/netflow/NetflowRelation.scala
+++ src/main/scala/com/github/sadikovi/spark/netflow/NetflowRelation.scala
@@ -22,14 +22,12 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.Logging
 import org.apache.spark.rdd.{RDD, UnionRDD}
 import org.apache.spark.sql.{SQLContext, Row}
-import org.apache.spark.sql.sources.{DataSourceRegister, HadoopFsRelation, HadoopFsRelationProvider, OutputWriterFactory}
+import org.apache.spark.sql.sources.{HadoopFsRelation, HadoopFsRelationProvider, OutputWriterFactory}
 import org.apache.spark.sql.types.{StructType, StructField, IntegerType}
 
 import com.github.sadikovi.spark.rdd.{NetflowFileRDD, NetflowMetadata}
 
-class DefaultSource extends HadoopFsRelationProvider with DataSourceRegister {
-
-  override def shortName(): String = "netflow"
+class DefaultSource extends HadoopFsRelationProvider {
 
   /**
    * Create relation for Netflow data. Options include path to the Netflow files, flow version
--- src/test/scala/com/github/sadikovi/spark/netflow/NetflowSuite.scala
+++ src/test/scala/com/github/sadikovi/spark/netflow/NetflowSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 sadikovi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.sadikovi.spark.netflow
 
 import java.io.IOException
@@ -60,7 +76,9 @@ class NetflowSuite extends UnitTestSpec with SparkLocal {
         load(s"file:${path3}").count()
     } catch {
       case se: SparkException =>
-        assert(se.getCause().isInstanceOf[UnsupportedOperationException])
+        val msg = se.getMessage()
+        assert(msg.contains("java.lang.UnsupportedOperationException: " +
+          "Corrupt Netflow file. Wrong magic number"))
       case other: Throwable => throw other
     }
   }
@@ -73,7 +91,8 @@ class NetflowSuite extends UnitTestSpec with SparkLocal {
       df.select("srcip").count()
     } catch {
       case se: SparkException =>
-        assert(se.getCause().getMessage() === "Unexpected EOF")
+        val msg = se.getMessage()
+        assert(msg.contains("java.lang.IllegalArgumentException: Unexpected EOF"))
       case other: Throwable => throw other
     }
   }
