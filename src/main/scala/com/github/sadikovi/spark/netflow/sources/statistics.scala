587bf9a66948bea6c55a2edeac852a9a180b6f06
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2016-04-21 20:21:01 +1200

    add attribute for statistics

--- src/main/scala/com/github/sadikovi/spark/netflow/sources/statistics.scala
+++ src/main/scala/com/github/sadikovi/spark/netflow/sources/statistics.scala
@@ -16,6 +16,10 @@
 
 package com.github.sadikovi.spark.netflow.sources
 
+import java.util.{HashSet => JHashSet}
+
+import scala.reflect.ClassTag
+
 import org.apache.hadoop.fs.{Path => HadoopPath}
 
 /**
@@ -59,4 +63,92 @@ case class StatisticsPathResolver(maybeRoot: Option[String]) {
 
   /** Return statistics name based on original file name */
   private def getStatisticsName(fileName: String): String = s".statistics-$fileName"
+}
+
+/**
+ * [[Attribute]] interface to collect and check statistics. Included support of different
+ * combinations of collected parameters: count, min/max, and set of values through bit vector. Here
+ * are some common flags: 7 - enable all parameters, 1 - enable count, 6 - enable min/max and set.
+ * In order to create attribute comparison function is required similar the `lt` function used in
+ * `sortWith` method. Name must unique to the attribute.
+ */
+case class Attribute[T](
+    name: String,
+    lt: (T, T) => Boolean, flags: Byte)(implicit tag: ClassTag[T]) {
+  private var count: Long = if (isCountEnabled) 0 else Long.MinValue
+  private var min: T = _
+  private var max: T = _
+  private var set: JHashSet[T] = if (isSetEnabled) new JHashSet() else null
+
+  /** Add value to the attribute, it automatically checks all available modes */
+  def addValue(value: T): Unit = {
+    if (isCountEnabled) {
+      count += 1
+    }
+
+    if (isMinMaxEnabled) {
+      if (min == null || !lt(min, value)) {
+        min = value
+      }
+
+      if (max == null || lt(max, value)) {
+        max = value
+      }
+    }
+
+    if (isSetEnabled) {
+      set.add(value)
+    }
+  }
+
+  private def isEnabled(flag: Byte): Boolean = {
+    (flags & flag) == flag
+  }
+
+  /** Check if count is collected by this attribute */
+  def isCountEnabled(): Boolean = isEnabled(1)
+
+  /** Check if min/max is collected by this attribute */
+  def isMinMaxEnabled(): Boolean = isEnabled(2)
+
+  /** Check if set is collected by this attribute */
+  def isSetEnabled(): Boolean = isEnabled(4)
+
+  /** Get count, if mode is enabled, otherwise None */
+  def getCount(): Option[Long] = {
+    if (isCountEnabled) Some(count) else None
+  }
+
+  /** Check if value is in min-max range, if mode is enabled, otherwise None */
+  def containsInRange(value: T): Option[Boolean] = {
+    if (isMinMaxEnabled) Some(!lt(value, min) && !lt(max, value)) else None
+  }
+
+  /** Check if value is in set, if mode is enabled, otherwise None */
+  def containsInSet(value: T): Option[Boolean] = {
+    if (isSetEnabled) Some(set.contains(value)) else None
+  }
+
+  /** Update count with value */
+  private[sources] def setCount(value: Long): Unit = {
+    require(isCountEnabled, s"Count mode is disabled, bit flags: $flags")
+    count = value
+  }
+
+  /** Update min/max directly with values */
+  private[sources] def setMinMax(minValue: T, maxValue: T): Unit = {
+    require(isMinMaxEnabled, s"Min-max mode is disabled, bit flags: $flags")
+    min = minValue
+    max = maxValue
+  }
+
+  /** Update set directly with value */
+  private[sources] def setSet(setValue: JHashSet[T]): Unit = {
+    require(isSetEnabled, s"Set mode is disabled, bit flags: $flags")
+    set = setValue
+  }
+
+  override def toString(): String = {
+    s"${getClass().getCanonicalName}, name: $name, bit flags: $flags"
+  }
 }
--- src/test/scala/com/github/sadikovi/spark/netflow/sources/StatisticsSuite.scala
+++ src/test/scala/com/github/sadikovi/spark/netflow/sources/StatisticsSuite.scala
@@ -16,6 +16,7 @@
 
 package com.github.sadikovi.spark.netflow.sources
 
+import java.util.{HashSet => JHashSet}
 import com.github.sadikovi.testutil.UnitTestSpec
 
 class StatisticsSuite extends UnitTestSpec {
@@ -54,6 +55,82 @@ class StatisticsSuite extends UnitTestSpec {
     val resolver = StatisticsPathResolver(None)
     intercept[IllegalArgumentException] {
       resolver.getStatisticsPath("")
+    }
+  }
+
+  test("create and update attribute") {
+    val attr = Attribute[Int]("a", _ < _, 7)
+    for (i <- 1 to 3) {
+      attr.addValue(i)
+    }
+    attr.getCount() should be (Some(3))
+    attr.containsInRange(2) should be (Some(true))
+    attr.containsInSet(2) should be (Some(true))
+  }
+
+  test("get value from attribute for range mode") {
+    val attr = Attribute[Int]("a", _ < _, 2)
+    attr.addValue(3)
+    attr.addValue(5)
+    attr.containsInRange(2) should be (Some(false))
+    attr.containsInRange(3) should be (Some(true))
+    attr.containsInRange(4) should be (Some(true))
+    attr.containsInRange(5) should be (Some(true))
+    attr.containsInRange(6) should be (Some(false))
+  }
+
+  test("get value from attribute for set mode") {
+    val attr = Attribute[Int]("a", _ < _, 4)
+    attr.addValue(3)
+    attr.addValue(5)
+    attr.containsInSet(2) should be (Some(false))
+    attr.containsInSet(3) should be (Some(true))
+    attr.containsInSet(4) should be (Some(false))
+    attr.containsInSet(5) should be (Some(true))
+    attr.containsInSet(6) should be (Some(false))
+  }
+
+  test("get value from attribute for count mode") {
+    val attr = Attribute[Int]("a", _ < _, 1)
+    attr.getCount() should be (Some(0))
+    attr.addValue(3)
+    attr.addValue(3)
+    attr.getCount() should be (Some(2))
+  }
+
+  test("get value from attribute for incorrect mode") {
+    val attr = Attribute[Int]("a", _ < _, 8)
+    attr.getCount() should be (None)
+    attr.containsInRange(1) should be (None)
+    attr.containsInSet(1) should be (None)
+  }
+
+  test("update values of attribute directly") {
+    val attr = Attribute[Int]("a", _ < _, 7)
+    attr.setCount(10)
+    attr.setMinMax(2, 5)
+    val set = new JHashSet[Int]()
+    set.add(2)
+    set.add(5)
+    attr.setSet(set)
+
+    attr.getCount() should be (Some(10))
+    attr.containsInRange(2) should be (Some(true))
+    attr.containsInSet(2) should be (Some(true))
+  }
+
+  test("fail when updating attribute for unset mode") {
+    val attr = Attribute[Int]("a", _ < _, 8)
+    intercept[IllegalArgumentException] {
+      attr.setCount(1)
+    }
+
+    intercept[IllegalArgumentException] {
+      attr.setMinMax(1, 2)
+    }
+
+    intercept[IllegalArgumentException] {
+      attr.setSet(new JHashSet[Int]())
     }
   }
 }
